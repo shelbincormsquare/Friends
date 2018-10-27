e53e694e2673ac4e6d1ca184dc9538fa576d634f
shelbincormsquare <shelbin@cormsquare.com>
2018-10-27 23:34:20 +0530

    Add project files.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Shelbin", LastName = "Kidangan" };
+            yield return new Friend { FirstName = "Shimnas", LastName = "Sidque" };
+            yield return new Friend { FirstName = "Shreya", LastName = "Uday" };
+            yield return new Friend { FirstName = "Niket", LastName = "Soni" };
+        }
+    }
+}
